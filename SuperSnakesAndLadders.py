a118da78a0db3d05b97d5281ff6441a0f0dce2f9
David K Peng <davekpeng@gmail.com>
2019-04-16 10:52:29 -0400

    Added Stuff

---
+++ SuperSnakesAndLadders.py
@@ -0,0 +1,24 @@
+import sys
+import random
+import math
+import os
+import pygame
+from pygame.locals import *
+
+WIDTH = 720
+HEIGHT = 720
+
+
+
+
+
+
+def main():
+    """Main Game Code"""
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Super Duper Snakes and Ladders Deluxe 345/2 Days X3 Ultra')
+
+
+if __name__ == '__main__':
+    main()
